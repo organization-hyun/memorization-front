3df057f3df8e096bce8bb877409716d22223d857
hyunjuuun <hyunjun86120@gmail.com>
2023-09-28 19:36:02 +0900

    constructor 제거

--- src/practice/Gugudan.js
+++ src/practice/Gugudan.js
@@ -1,21 +1,18 @@
 import React from "react";
 
 class Gugudan extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            first: Math.ceil(Math.random() * 9),
-            second: Math.ceil(Math.random() * 9),
-            value: '',
-            result: '',
-        };
-    }
+    state = {
+        first: Math.ceil(Math.random() * 9),
+        second: Math.ceil(Math.random() * 9),
+        value: '',
+        result: '',
+    };
 
     onSubmit = (e) => {
         e.preventDefault();
         if (parseInt(this.state.value) === this.state.first * this.state.second) {
             this.setState({
-                result: '정답',
+                result: this.state.first * this.state.second + ' 정답',
                 first: Math.ceil(Math.random() * 9),
                 second: Math.ceil(Math.random() * 9),
                 value: '',
@@ -34,15 +31,15 @@ class Gugudan extends React.Component {
 
     render() {
         return (
-            <div>
+            <>
                 <div>{this.state.first}곱하기{this.state.second}는?</div>
                 <form onSubmit={this.onSubmit}>
                     <input type="number" value={this.state.value}
                            onChange={this.onChange}/>
-                    <button>입력!</button>
+                    <button type="submit">입력!</button>
                 </form>
                 <div>{this.state.result}</div>
-            </div>
+            </>
         );
     }
 }
