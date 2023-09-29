3c70bf595e76dc1629c45881688d7c291ed5d5d9
hyunjuuun <hyunjun86120@gmail.com>
2023-09-29 11:56:25 +0900

    React Hooks 사용

--- src/practice/Gugudan.js
+++ src/practice/Gugudan.js
@@ -1,57 +1,41 @@
-import React from "react";
+import React, {useRef} from "react";
 
-class Gugudan extends React.Component {
-    state = {
-        first: Math.ceil(Math.random() * 9),
-        second: Math.ceil(Math.random() * 9),
-        value: '',
-        result: '',
+const Gugudan = () => {
+    const [first, setFirst] = React.useState(Math.ceil(Math.random() * 9));
+    const [second, setSecond] = React.useState(Math.ceil(Math.random() * 9));
+    const [value, setValue] = React.useState('');
+    const [result, setResult] = React.useState('');
+    const inputRef = React.useRef(null);
+
+    const onChangeInput = (e) => {
+        setValue(e.target.value);
     };
 
-    onSubmit = (e) => {
+    const onSubmitForm = (e) => {
         e.preventDefault();
-        if (parseInt(this.state.value) === this.state.first * this.state.second) {
-            this.setState((prevState) => {
-                return {
-                    result: '정답: ' + prevState.value,
-                    first: Math.ceil(Math.random() * 9),
-                    second: Math.ceil(Math.random() * 9),
-                    value: '',
-                };
-            });
+        if (parseInt(value) === first * second) {
+            setResult('정답: ' + value);
+            setFirst(Math.ceil(Math.random() * 9));
+            setSecond(Math.ceil(Math.random() * 9));
+            setValue('');
+            inputRef.current.focus();
         } else {
-            this.setState({
-                result: '땡',
-                value: '',
-            });
+            setResult('땡');
+            setValue('');
+            inputRef.current.focus();
         }
-        this.input.focus();
     };
 
-    onChange = (e) => {
-        this.setState({value: e.target.value})
-    };
-
-    onRefInput = (c) => {
-        this.input = c;
-    }
-
-    input;
-
-    render() {
-        console.log('렌더링');
-        return (
-            <>
-                <div>{this.state.first}곱하기{this.state.second}는?</div>
-                <form onSubmit={this.onSubmit}>
-                    <input ref={this.onRefInput} type="number" value={this.state.value}
-                           onChange={this.onChange}/>
-                    <button type="submit">입력!</button>
-                </form>
-                <div>{this.state.result}</div>
-            </>
-        );
-    }
+    return (
+        <>
+            <div>{first} 곱하기 {second}는?</div>
+            <form onSubmit={onSubmitForm}>
+                <input ref={inputRef} onChange={onChangeInput} value={value}/>
+                <button>입력!</button>
+            </form>
+            <div id="result">{result}</div>
+        </>
+    );
 }
 
 export default Gugudan;
