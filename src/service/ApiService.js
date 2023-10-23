c76cd37b66f129f56fd4f3c34cd9ed1d9e8ba869
hyunjuuun <hyunjun86120@gmail.com>
2023-10-23 19:52:37 +0900

    call function 수정

--- src/service/ApiService.js
+++ src/service/ApiService.js
@@ -1,6 +1,6 @@
 export async function call(url, method, data = {}) {
   // Default options are marked with *
-  const response = await fetch(url, {
+  return await fetch(url, {
     method: method, // *GET, POST, PUT, DELETE, etc.
     mode: "cors", // no-cors, *cors, same-origin
     cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
@@ -13,5 +13,4 @@ export async function call(url, method, data = {}) {
     referrerPolicy: "no-referrer", // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
     body: JSON.stringify(data), // body data type must match "Content-Type" header
   });
-  return await response.json(); // parses JSON response into native JavaScript objects
 }
