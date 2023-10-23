69dd15887056c69308e2a1fd121c1f93749e5e75
hyunjuuun <hyunjun86120@gmail.com>
2023-10-23 18:02:42 +0900

    체크 박스 선택된 term id state로 관리

--- src/common/TermsTable.jsx
+++ src/common/TermsTable.jsx
@@ -1,11 +1,34 @@
-import {Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow} from "@mui/material";
+import {Checkbox, Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow} from "@mui/material";
+import {IconButton} from "@material-ui/core";
+import {Delete} from "@material-ui/icons";
+import {useState} from "react";
 
 function TermsTable({headers, terms}) {
+
+  const [disabled, setDisabled] = useState(true);
+  const [selectedIds, setSelectedIds] = useState([]);
+
+  const handleCheckboxClick = (id) => {
+    const isIdSelected = selectedIds.includes(id);
+
+    if (isIdSelected) {
+      const newSelectedIds = selectedIds.filter((selectedId) => selectedId !== id);
+      setSelectedIds(newSelectedIds);
+    } else {
+      setSelectedIds((prev) => [...prev, id]);
+    }
+  };
+
   return (
     <TableContainer component={Paper}>
       <Table sx={{minWidth: 650}} aria-label="simple table">
         <TableHead>
           <TableRow>
+            <TableCell key={"checkboxArea"} align="center">
+              <IconButton aria-label="delete" disabled={disabled}>
+                <Delete/>
+              </IconButton>
+            </TableCell>
             {headers.map(header =>
               <TableCell key={headers.indexOf(header)} align="right">{header}</TableCell>)
             }
@@ -17,6 +40,9 @@ function TermsTable({headers, terms}) {
               key={term.id}
               sx={{'&:last-child td, &:last-child th': {border: 0}}}
             >
+              <TableCell align="center" size="small">
+                <Checkbox onClick={() => handleCheckboxClick(term.id)}/>
+              </TableCell>
               <TableCell align="right">{term.word}</TableCell>
               <TableCell align="right">{term.description}</TableCell>
               <TableCell align="right">{term.keywords[0]}</TableCell>
