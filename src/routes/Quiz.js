1eedf938247a273baac5c1914db221bea2bf83a7
hyunjuuun <hyunjun86120@gmail.com>
2023-10-24 20:51:37 +0900

    Quiz 조회 api 호출

--- src/routes/Quiz.js
+++ src/routes/Quiz.js
@@ -1,6 +1,26 @@
+import {useParams} from "react-router";
+import {API_BASE_URL} from "../app-config";
+import {useEffect, useState} from "react";
+
 function Quiz() {
+    const {id} = useParams();
+
+    const [quizzes, setQuizzes] = useState([]);
+
+    useEffect(() => {
+        getQuizzes();
+    }, [])
+
+    const getQuizzes = async () => {
+        const response = await ((await fetch(
+            `${API_BASE_URL}/glossaries/${id}/quizzes`
+        )).json());
+
+        setQuizzes(response.quizzes);
+    }
+
     return (
-        "This is Quiz!"
+        <div>Quiz</div>
     );
 }
 
