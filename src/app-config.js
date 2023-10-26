538b465387772ba7fba2154a1333ab63dab7c20e
hyunjuuun <hyunjun86120@gmail.com>
2023-10-26 18:44:17 +0900

    로컬에서는 백엔드 로컬 서버로 API_BASE_URL 설정

--- src/app-config.js
+++ src/app-config.js
@@ -1,3 +1,9 @@
-const backendHost = "https://port-0-memorization-12fhqa2llo2ulv8z.sel5.cloudtype.app";
+let backendHost = "https://port-0-memorization-12fhqa2llo2ulv8z.sel5.cloudtype.app";
+
+const hostname = window && window.location && window.location.hostname;
+
+if (hostname === "localhost") {
+    backendHost = "http://localhost:8080";
+}
 
 export const API_BASE_URL = `${backendHost}`;
